68f2b76305758205a82edde9071f0258e912edcb
Jamesson Faria <jamessonjr@gmail.com>
2020-04-18 15:58:51 -0300

    add project and library to project

--- projects/my-app-senior2/src/app/app.module.ts
+++ projects/my-app-senior2/src/app/app.module.ts
@@ -1,20 +1,16 @@
-import { MyLibraryModule } from './../../projects/my-library/src/lib/my-library.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MyLibraryModule
-  
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
