42492aeae19cdcb9327081b2ca4f053335eb7dce
Jamesson Faria <jamessonjr@gmail.com>
2020-04-19 12:28:25 -0300

    add i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app-senior';
+
+  numeroPessoas = 10;
+  genero = 'k';
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { MyLibraryModule } from './../../projects/my-library/src/lib/my-library.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { registerLocaleData } from '@angular/common';
+import localeBR from '@angular/common/locales/pt';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MyButtonComponent } from './my-button/my-button.component';
 
+registerLocaleData(localeBR); 
 
 @NgModule({
   declarations: [
@@ -20,7 +23,9 @@ import { MyButtonComponent } from './my-button/my-button.component';
     BrowserAnimationsModule
   
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
